e4d8929185622604050e334a194b9899936933d4
Sven Sommer <rob.hoffmann@posteo.de>
2020-05-22 17:13:27 +0000

    added default config files

--- app.js
+++ app.js
@@ -35,6 +35,6 @@ require("./routes/category.routes.js")(app);
 
 require("./routes/sorter.routes.js")(app);
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT || 3000, process.env.IP || '192.168.178.67', function(){
     console.log("Server has started!");
 });
